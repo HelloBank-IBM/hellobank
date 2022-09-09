56f9f8b896575af6ffa1616b39431154d10adae3
barroscruzc <barroscruzc@gmail.com>
2022-09-09 01:44:45 -0300

    Criado projeto com Spring Initializr

---
+++ src/main/java/com/hellobank/HellobankApplication.java
@@ -0,0 +1,13 @@
+package com.hellobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellobankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellobank/HellobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
