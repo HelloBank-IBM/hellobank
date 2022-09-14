fc874ae20cf725d037b15a123613559885c9157b
Walderney Oliveira Azevedo <85721450+wanoliveiraa@users.noreply.github.com>
2022-09-14 11:45:34 -0300

    atualizaçoes em cliente

--- src/main/java/com/hellobank/DAO/ContaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.hellobank.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.hellobank.Model.Conta;
-
-public interface ContaDAO extends CrudRepository< Conta, Integer> {
-    boolean existById(Integer id);
-}
