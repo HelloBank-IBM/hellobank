65a16da91c3bfa480d767c9eca34a40892c8009c
Walderney Oliveira Azevedo <85721450+wanoliveiraa@users.noreply.github.com>
2022-09-16 21:56:53 -0300

    att

--- src/main/java/com/hellobank/DAO/ContaDAO.java
+++ src/main/java/com/hellobank/DAO/ContaDAO.java
@@ -1,9 +1,15 @@
 package com.hellobank.DAO;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.hellobank.Model.Conta;
 
 public interface ContaDAO extends CrudRepository< Conta, Integer> {
     boolean existsById(Integer id);
+    @Query(value="select * from conta where numero_conta=:numero",nativeQuery = true) 
+    public Conta buscarNum(Integer numero);
 }
