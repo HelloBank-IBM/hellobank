c000598e624eb908185125bc6f795b5658635801
barroscruzc <barroscruzc@gmail.com>
2022-09-19 20:57:30 -0300

    Atualização página e método de extrato

--- src/main/java/com/hellobank/Controller/ViewController.java
+++ src/main/java/com/hellobank/Controller/ViewController.java
@@ -6,19 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.hellobank.DAO.TipoContaDAO;
-import com.hellobank.DAO.TransacaoDAO;
 import com.hellobank.Model.Cliente;
 import com.hellobank.Model.Conta;
 import com.hellobank.Model.TipoConta;
+import com.hellobank.Model.Transacao;
 import com.hellobank.Service.ClienteServicesImpl;
 import com.hellobank.Service.ContaServiceImpl;
+import com.hellobank.Service.TransacaoServiceImpl;
 
 @Controller
 @RequestMapping(path = "/hellobank/view")
@@ -31,18 +31,22 @@ public class ViewController {
     private ContaServiceImpl contaService;
 
     @Autowired
-    private TransacaoDAO transacaoDao;
-
-    @Autowired
     private TipoContaDAO tipoContaDao;
 
+    @Autowired
+    private TransacaoServiceImpl transacaoService;
+
+    /* Landing Page */
     @GetMapping("/index")
     public String index() {
         return "landing_page";
     }
 
-    @GetMapping("/inicio")
-    public String inicio(Cliente cliente, Model model){
+    /* Tela inicial da Aplicação */
+    @GetMapping("/inicio/{idCliente}")
+    public String inicio(@PathVariable Integer idCliente, Model model){
+        Cliente cliente = clienteService.buscarPeloId(idCliente);
+        model.addAttribute("cliente", cliente);
         return "inicio";
     }
 
@@ -109,8 +113,11 @@ public class ViewController {
         return "form_cadastro";
     }
 
+    /* Funcionalidades da Aplicação */
+
+    /* Saldo */
     @GetMapping("/saldo/{idCliente}")
-    public String saldoGet(@PathVariable Integer idCliente, Model model){
+    public String saldo(@PathVariable Integer idCliente, Model model){
         ArrayList<Conta> contas = contaService.buscarPeloIdCliente(idCliente);
         Cliente cliente = clienteService.buscarPeloId(idCliente);
         model.addAttribute("contas", contas);
@@ -118,4 +125,63 @@ public class ViewController {
         return "saldo";
     }
 
+    /* Extrato - verbo http GET */
+    @GetMapping("/extrato/{numeroConta}")
+    public String extratoGet(@PathVariable Integer numeroConta, Model model){
+        Conta conta = contaService.buscarPeloNumero(numeroConta);
+        Cliente cliente = conta.getCliente();
+        ArrayList<Transacao> transacoes = transacaoService.extrato(conta.getId());
+        ArrayList<Conta> contas = contaService.buscarPeloIdCliente(cliente.getId());
+        Conta formConta = new Conta();
+
+        model.addAttribute("transacoes", transacoes);
+        model.addAttribute("cliente", cliente);
+        model.addAttribute("conta", conta);
+        model.addAttribute("contas", contas);
+        model.addAttribute("formConta", formConta);
+        return "extrato";
+    }
+
+    @GetMapping("/extrato/cliente/{idCliente}")
+    public String extrato(@PathVariable Integer idCliente, Model model){
+        Cliente cliente = clienteService.buscarPeloId(idCliente);
+        ArrayList<Conta> contas = contaService.buscarPeloIdCliente(idCliente);
+        Integer numeroConta = 0;
+        for (Conta c : contas){
+            if (c.getTipo().getCodigo() == 1){
+                numeroConta = c.getNumeroConta();
+            };
+        }
+        Conta conta = contaService.buscarPeloNumero(numeroConta);
+
+        ArrayList<Transacao> transacoes = transacaoService.extrato(conta.getId());
+        Conta formConta = new Conta();
+
+        model.addAttribute("transacoes", transacoes);
+        model.addAttribute("cliente", cliente);
+        model.addAttribute("conta", conta);
+        model.addAttribute("contas", contas);
+        model.addAttribute("formConta", formConta);
+        return "extrato";
+    }
+
+
+
+    /* Extrato - verbo http POST */
+    @PostMapping("/extrato")
+    public String extratoPost(Conta formConta, Model model){
+        Conta conta = contaService.buscarPeloNumero(formConta.getNumeroConta());
+        Cliente cliente = conta.getCliente();
+        ArrayList<Transacao> transacoes = transacaoService.extrato(conta.getId());
+        ArrayList<Conta> contas = contaService.buscarPeloIdCliente(cliente.getId());
+        formConta = new Conta();
+        model.addAttribute("transacoes", transacoes);
+        model.addAttribute("cliente", cliente);
+        model.addAttribute("conta", conta);
+        model.addAttribute("contas", contas);
+        model.addAttribute("formConta", formConta);
+        return "extrato";
+    }
+
+    /* Extrato - POST */
 }
